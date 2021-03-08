8f3063d5bf2e6c113abcdb1321a7daad6b85780b
nabla27 <nabla3.1415@gmail.com>
2021-03-09 00:19:53 +0900

    Update display.sh: add -s option in read command

--- System_ver3/display.sh
+++ System_ver3/display.sh
@@ -81,7 +81,7 @@ do
 	cat $PATH_file_show | sed -n ${inf},${sup}p
 
 	#キー入力待機
-	read -n 1 _getch
+	read -s -n 1 _getch
 
 	#キー入力によるパラメータ変更
 	case "$_getch" in
@@ -107,20 +107,6 @@ do
 		;;
 		:)
 		mode="Command_Line" && echo "Command_Line" > $PATH_mode; exit
-		#echo -e "${C_mode}< Command Line >${Cend}"
-		#read _getcher
-		#if expr "$_getcher" : "[0-9]*$" >&/dev/null; then
-		#	mvd="`cat $PATH_pwd`/`cat $PATH_direct_list | sed -n ${_getcher}p`"
-		#	if [ -f "$mvd" ]; then cat "$mvd"; read -n 1 a
-		#	elif [ -d "$mvd" ]; then echo "→" > $PATH_file_show; cd $mvd
-		#	else echo "cannot operate"; fi
-		#else
-		#	if [[ "$_getcher" = cd* ]]; then echo "→" > $PATH_file_show; fi
-		#	$_getcher
-		#	read -n 1 a
-		#fi
-		#echo `pwd` > $PATH_pwd
-		#exit
 		;;
 		r)
 		mode="menu" && echo "menu" > $PATH_mode
@@ -129,7 +115,7 @@ do
 		[1-9])
 		echo "--------------------"
 		mvd="`cat $PATH_pwd`/`cat $PATH_direct_list | sed -n ${_getch}p`"
-		if [ -f "$mvd" ]; then cat "$mvd"; read -n 1 a
+		if [ -f "$mvd" ]; then cat "$mvd"; read -n 1
 		elif [ -d "$mvd" ]; then echo "→" > $PATH_file_show; cd "$mvd"
 		else echo  "${_getch}'s file is not accessible."; read -n 1; fi
 		echo `pwd` > $PATH_pwd; exit
