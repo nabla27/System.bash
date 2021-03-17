9aba7749e6bd7e9e6d3137016a2669b0f41e8138
nabla27 <nabla3.1415@gamil.com>
2021-03-17 20:17:04 +0900

    Update cmd_search.sh

--- Test_dir/cmd_search.sh
+++ Test_dir/cmd_search.sh
@@ -14,11 +14,56 @@ Cend="\e[m"; Cor="`sed -n 12p $PATH_Set`"
 
 sp[1]="${C_cor}${Cor}${Cend}"; sp[2]="  "; sp[3]="  "; sp[4]="  "; sp[5]="  "; sp[6]="  "; sp[7]="  "
 sp4[1]="  "; sp4[2]="  "; sp[3]="  "; sp[4]="  "; sp[5]="  "
+s_cmd="none"; s_opt="none"; s_key1="none"; s_key2="none"; s_key3="none"; s_key4="none"; s_key5="none"
 num=1; num4=0
-
-
-
 mode="cmd_search"
+
+
+function cmd_search(){
+	IFS_BACKUP=$IFS
+	IFS=$'\n'
+	local CMD="$1"
+	local OPT="$2"
+	local KEY[1]="$3"
+	local KEY[2]="$4"
+	local KEY[3]="$5"
+	local KEY[4]="$6"
+	local KEY[5]="$7"
+	local line_num=1
+	local tf_show="false"
+	for line in `cat $PATH_cmdlist`
+	do
+		if [ "$CMD" != "none" ]; then
+			if [[ "$line" = ">>${CMD}<<" ]]; then
+				tf_show="true"
+			elif [[ "$line" = ">><<" ]]; then
+				tf_show="false"
+			fi
+		fi
+	
+	case "${line}" in
+		*"${KEY[1]}"*)
+			;;
+		*"${KEY[2]}"*)
+			;;
+		*"${KEY[3]}"*)
+			;;
+		*"${KEY[4]}"*)
+			;;
+		*"${KEY[5]}"*)
+			;;
+		*)
+			tf_show=false
+		;;
+	esac
+
+	
+		if [ $tf_show = true ]; then
+			echo "${line}"
+		fi
+	done
+	IFS=$IFS_BACKUP
+}
 
 while [ $mode = "cmd_search" ]
 do
@@ -57,17 +102,30 @@ do
 		d)
 		if [ $num -eq 4 ]; then num4=$((num4+1))
 		elif [ $num -eq 1 ]; then
-			echo "1"; read -s -n 1
+			echo "Enter the command name"
+			read _cmd_name
+			s_cmd="${_cmd_name}"
 		elif [ $num -eq 2 ]; then
-			echo "2"; read -s -n 1
+			echo "Enter the option name"
+			read _option_name
+			s_opt="${_option_name}"
 		elif [ $num -eq 3 ]; then
-			echo "3"; read -s -n 1
+			echo "Enter the key words"
+			read _keyword
+			s_key1=`echo "${_keyword}" | awk '{print $1}'`
+			s_key2=`echo "${_keyword}" | awk '{print $2}'`
+			s_key3=`echo "${_keyword}" | awk '{print $3}'`
+			s_key4=`echo "${_keyword}" | awk '{print $4}'`
+			s_key5=`echo "${_keyword}" | awk '{print $5}'`
 		elif [ $num -eq 5 ]; then
-			echo "5"; read -s -n 1
+			s_cmd="none"; s_opt="none"; s_key1="none"; s_key2="none"; s_key3="none"; s_key4="none"; s_key5="none"
 		elif [ $num -eq 6 ]; then
-			echo "6"; read -s -n 1
+			echo -e "********************${C_title}result${Cend}********************"
+			cmd_search "${s_cmd}" "${s_opt}" "${s_key1}" "${s_key2}" "${s_key3}" "${s_key4}" "${s_key5}"
+			echo -e "**********************************************"
+			read -s -n 1
 		elif [ $num -eq 7 ]; then
-			echo "7"; read -s -n 1
+			echo "list"; read -s -n 1
 		fi
 		;;
 	esac
